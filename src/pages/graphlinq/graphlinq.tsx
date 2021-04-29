f74192e064b79728094842878ed740b61aba78ee
John R. Bussiculo <joanas@gmail.com>
2021-04-29 19:52:50 -0400

    adding latest trades back to glq page

--- src/pages/graphlinq/graphlinq.tsx
+++ src/pages/graphlinq/graphlinq.tsx
@@ -128,6 +128,88 @@ const GraphLinqContent: React.FC<GlqProps> = ({ }) => {
               </div>
             </div>
           </div>
+          <div className="blc cl100">
+            <div>
+              <div className="top">
+                <small>Last 663 trades</small>
+                <h2>
+                  <strong>GLQ Trades</strong>
+                </h2>
+              </div>
+              <div className="tbl">
+                <div className="tblc">
+                  <div>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Date</th>
+                          <th>Type</th>
+                          <th>Price USD</th>
+                          <th>Price ETH</th>
+                          <th>Amount GLQ</th>
+                          <th>Total ETH</th>
+                          <th>Marker</th>
+                          <th>Other</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>2021-03-31 19:03:26</td>
+                          <td className="gre">Buy</td>
+                          <td>$0.07410636</td>
+                          <td>0.000004002</td>
+                          <td>5,000.00</td>
+                          <td>0.200009925</td>
+                          <td><a href="">...bad8z8ddd4fcc87</a></td>
+                          <td>-</td>
+                        </tr>
+                        <tr>
+                          <td>2021-03-31 19:03:26</td>
+                          <td className="red">Sell</td>
+                          <td>$0.07410636</td>
+                          <td>0.000004002</td>
+                          <td>5,000.00</td>
+                          <td>0.200009925</td>
+                          <td><a href="">...bad8z8ddd4fcc87</a></td>
+                          <td>-</td>
+                        </tr>
+                        <tr>
+                          <td>2021-03-31 19:03:26</td>
+                          <td className="gre">Buy</td>
+                          <td>$0.07410636</td>
+                          <td>0.000004002</td>
+                          <td>5,000.00</td>
+                          <td>0.200009925</td>
+                          <td><a href="">...bad8z8ddd4fcc87</a></td>
+                          <td>-</td>
+                        </tr>
+                        <tr>
+                          <td>2021-03-31 19:03:26</td>
+                          <td className="gre">Buy</td>
+                          <td>$0.07410636</td>
+                          <td>0.000004002</td>
+                          <td>5,000.00</td>
+                          <td>0.200009925</td>
+                          <td><a href="">...bad8z8ddd4fcc87</a></td>
+                          <td>-</td>
+                        </tr>
+                        <tr>
+                          <td>2021-03-31 19:03:26</td>
+                          <td className="gre">Buy</td>
+                          <td>$0.07410636</td>
+                          <td>0.000004002</td>
+                          <td>5,000.00</td>
+                          <td>0.200009925</td>
+                          <td><a href="">...bad8z8ddd4fcc87</a></td>
+                          <td>-</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
